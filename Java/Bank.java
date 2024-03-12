9a09729d97a3eb1c8de79008cdd2d27ed88b5b38
Mustafa Yusuf <mydasdemir229@gmail.com>
2024-03-12 14:06:20 +0300

    First Examples
    Bank, Calculate factorial, number of PI and Sinus.

---
+++ Java/Bank.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Bank {
+
+    public static void main(String[] args) 
+    {
+        float money = 1000;
+        boolean condition = true;
+
+        while (condition) {
+
+        Scanner value = new Scanner(System.in);
+        System.out.println("Enter a number : \n1. Show balance\n2. Deposit money \n3. Withdraw money \n4. Log out of the system\n");
+
+        int num = value.nextInt();
+
+            switch (num) {
+                case 1:
+                    Goster(money);
+                    break;
+
+                case 2:
+                    money += Yatir();
+                    break;
+
+                case 3:
+                    money -= Cek(money);
+                    break;
+
+                default:
+                    Cikis(condition);
+                    break;
+            }
+            value.close();
+        }
+    }
+
+
+    public static void Goster(float money)
+    {
+        System.out.println(money);
+    }
+    
+    public static float Yatir()
+    {
+        Scanner value = new Scanner(System.in);
+        System.out.println("Enter value : ");
+
+        float num = value.nextFloat();
+        value.close();
+
+        return num;
+    }
+
+    public static float Cek(float mon)
+    {
+        Scanner value = new Scanner(System.in);
+        System.out.println("Enter value : ");
+
+        float num = value.nextFloat();
+        value.close();
+
+        if(num > mon)
+        {
+            System.out.println("Insufficient funds.\n");
+            return 0.0f;
+        }
+        else
+        {
+            return num;
+        }
+    }
+
+    public static void Cikis(boolean condition)
+    {
+        condition = false;
+    }
+}
---
+++ Java/Sinus.java
@@ -0,0 +1,26 @@
+public class Sinus {
+    public static void main(String[] args) throws InterruptedException
+    {
+       int freaquency = 10;
+
+       for(int degree = 0; degree <= 360; degree++)
+       {
+            double radyan =Math.toRadians(degree);
+            double sin = Math.sin(freaquency*radyan);
+
+            Print(sin);
+
+            System.out.println();
+            Thread.sleep(50);
+        }
+    }
+
+    public static void Print(double sin)
+    {
+        int numberOfStars = (int)(Math.abs(sin)*15);
+        for(int i = 0; i < numberOfStars; i++)
+        {
+            System.out.print("*");
+        }
+    }
+}
